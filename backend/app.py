d480ac6764a76bf743da4179bf4ca2be8c47549a
Marios Chrysanthou <marios.chrysanthou@gmail.com>
2025-04-23 11:43:20 +0300

    Fix: remove app.run() for Gunicorn deployment

--- backend/app.py
+++ backend/app.py
@@ -102,12 +102,8 @@ def reset():
         "log": learner.get_log()
     })
 
-# -------------------------------
-# Run the app
-# -------------------------------
 
-if __name__ == '__main__':
-    import os
+# NO app.run() here at all!
 
-port = int(os.environ.get("PORT", 5000))
-app.run(host="0.0.0.0", port=port, debug=True)
+# Flask will be served by Gunicorn using:
+# gunicorn app:app
